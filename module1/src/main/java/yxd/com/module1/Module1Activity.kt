a2b521ddb651d162d141b637bb1fd45f67a2dd1a
yexudong <ye850570138@sina.com>
2019-04-12 19:24:44 +0800

    commit base lib

--- module1/src/main/java/yxd/com/module1/Module1Activity.kt
+++ module1/src/main/java/yxd/com/module1/Module1Activity.kt
@@ -3,12 +3,17 @@ package yxd.com.module1
 import android.os.Bundle
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.kotlinlib.activity.AbstractActivity
+import com.kotlinlib.new_fragment.FragmentationUtils
+import me.yokeyword.fragmentation.SupportFragment
 
 @Route(path = "/test1/module1")
 class Module1Activity : AbstractActivity() {
 
     override fun init(bundle: Bundle?) {
         setContentView(R.layout.activity_module1)
+
+        FragmentationUtils<SupportFragment>(this, R.id.flContainer, Module1Fragment())
+
     }
 
 }
--- module2/src/main/java/yxd/com/module2/Module2Activity.kt
+++ module2/src/main/java/yxd/com/module2/Module2Activity.kt
@@ -4,12 +4,16 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 import com.alibaba.android.arouter.facade.annotation.Route
+import com.kotlinlib.activity.AbstractActivity
+import com.kotlinlib.new_fragment.FragmentationUtils
+import me.yokeyword.fragmentation.SupportFragment
 
 @Route(path = "/test2/module2")
-class Module2Activity : AppCompatActivity() {
+class Module2Activity : AbstractActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun init(bundle: Bundle?) {
         setContentView(R.layout.activity_module2)
+        FragmentationUtils<SupportFragment>(this, R.id.flContainer, ModuleFragment2())
     }
+
 }
